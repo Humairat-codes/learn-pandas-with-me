190a9bb52b72d15d3fb6fc2f551ea7b175832ea8
Humairat-codes <humairat.codes@gmail.com>
2026-09-01 08:35:08 +0530

    Series in python

--- Series.py
+++ Series.py
@@ -1,7 +1,38 @@
 import pandas as pd
-
+#list data
 data = [100,102,104,200,202]
 
 series = pd.Series(data,index=['a','b','c','d','e'])
+#update series using loc[]
+series['e'] += 200
+# print(series)
 
 
+
+#dictionary data , keys act like indices
+calories = {"Day 1":1898,"Day 2":1992,"Data 3":2080}
+ds = pd.Series(calories)
+#update series using loc[] 
+ds.loc["Day 1"] += 100
+print(ds.iloc[0])
+print(ds.loc["Day 2"])
+print(ds)
+# filter by value
+print(ds[ds >= 2000])
+print(ds[ds<2000])
+
+
+# Home work
+pokemon = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard"]
+ps = pd.Series(pokemon)
+print(ps)
+
+student_record = {
+    "Name" : "ABCD",
+    "Roll No." : 43,
+    "Grades" : ['A+','A','A+','A'],
+    "Languages known":['English','German'],
+    "Hobbies" : ['coding','sleeping','exercise','reading']
+}
+sd = pd.Series(student_record)
+print(sd)
